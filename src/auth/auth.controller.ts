3ca9cc4d6572f07e347b977e2bf764fc021a47c6
MJsnater <montassirejerrari@gmail.com>
2023-04-30 18:54:25 +0100

    authModule

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Req, Res, UseGuards ,Request } from '@nestjs/common';
+import { Request as _Request, Response } from 'express';
 import { signInDto } from 'src/dtos/SignInDto';
+import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -7,8 +9,18 @@ export class AuthController {
     constructor(private authService: AuthService) { }
     @HttpCode(HttpStatus.OK)
     @Post('login')
-    signIn(@Body() signInDto: signInDto) {
-        return this.authService.signIn(signInDto.name, signInDto.password);
+    async signIn(@Body() signInDto: signInDto , @Res() res  : Response ,@Req() req  : _Request) {
+        let token    =await this.authService.signIn(signInDto.name, signInDto.password);
+        console.log(req.headers.authorization);
+        
+        res.setHeader('Authorization', `Bearer ${token.access_token}`);
+        res.send( token );
+        return 
+    }
+    @UseGuards(AuthGuard)
+    @Get('profile')
+    getProfile(@Request() req) {
+      return req.user;
     }
 
 }
